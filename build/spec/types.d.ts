714f41fac2486c1061d0a96f1e151c3ae8c5f6a3
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2023-11-06 22:12:11 +0530

    Included build

---
+++ build/spec/types.d.ts
@@ -0,0 +1,65 @@
+export declare const types: {
+    AppId: string;
+    DataLookupIndexItem: {
+        appId: string;
+        start: string;
+    };
+    DataLookup: {
+        size: string;
+        index: string;
+    };
+    KateCommitment: {
+        rows: string;
+        cols: string;
+        commitment: string;
+        dataRoot: string;
+    };
+    V1HeaderExtension: {
+        appLookup: string;
+        commitment: string;
+    };
+    HeaderExtension: {
+        _enum: {
+            V1: string;
+        };
+    };
+    DaHeader: {
+        parentHash: string;
+        number: string;
+        stateRoot: string;
+        extrinsicsRoot: string;
+        digest: string;
+        extension: string;
+    };
+    Header: string;
+    CheckAppIdExtra: {
+        appId: string;
+    };
+    CheckAppIdTypes: {};
+    CheckAppId: {
+        extra: string;
+        types: string;
+    };
+    BlockLength: {
+        max: string;
+        cols: string;
+        rows: string;
+        chunkSize: string;
+    };
+    PerDispatchClass: {
+        normal: string;
+        operational: string;
+        mandatory: string;
+    };
+    DataProof: {
+        root: string;
+        proof: string;
+        numberOfLeaves: string;
+        leaf_index: string;
+        leaf: string;
+    };
+    Cell: {
+        row: string;
+        col: string;
+    };
+};
---
+++ build/spec/types.js
@@ -0,0 +1,68 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.types = void 0;
+exports.types = {
+    AppId: "Compact<u32>",
+    DataLookupIndexItem: {
+        appId: "AppId",
+        start: "Compact<u32>",
+    },
+    DataLookup: {
+        size: "Compact<u32>",
+        index: "Vec<DataLookupIndexItem>",
+    },
+    KateCommitment: {
+        rows: "Compact<u16>",
+        cols: "Compact<u16>",
+        commitment: "Vec<u8>",
+        dataRoot: "H256",
+    },
+    V1HeaderExtension: {
+        appLookup: "DataLookup",
+        commitment: "KateCommitment",
+    },
+    HeaderExtension: {
+        _enum: {
+            V1: "V1HeaderExtension",
+        },
+    },
+    DaHeader: {
+        parentHash: "Hash",
+        number: "Compact<BlockNumber>",
+        stateRoot: "Hash",
+        extrinsicsRoot: "Hash",
+        digest: "Digest",
+        extension: "HeaderExtension",
+    },
+    Header: "DaHeader",
+    CheckAppIdExtra: {
+        appId: "AppId",
+    },
+    CheckAppIdTypes: {},
+    CheckAppId: {
+        extra: "CheckAppIdExtra",
+        types: "CheckAppIdTypes",
+    },
+    BlockLength: {
+        max: "PerDispatchClass",
+        cols: "Compact<u32>",
+        rows: "Compact<u32>",
+        chunkSize: "Compact<u32>",
+    },
+    PerDispatchClass: {
+        normal: "u32",
+        operational: "u32",
+        mandatory: "u32",
+    },
+    DataProof: {
+        root: "H256",
+        proof: "Vec<H256>",
+        numberOfLeaves: "Compact<u32>",
+        leaf_index: "Compact<u32>",
+        leaf: "H256",
+    },
+    Cell: {
+        row: "u32",
+        col: "u32",
+    },
+};
